99f0ff252af1472d883b74f9378a414b5955e6ee
Jewls Krueger <jewls@Jewlss-MacBook-Pro.local>
2019-12-13 13:37:52 -0800

    added application model

--- api/models/application.php
+++ api/models/application.php
@@ -1,3 +1,18 @@
 <?php
 
+class Application {
+  public $user;
+  public $companyName;
+  public $jobTitle;
+  public $jobLink;
+  public $appStatus;
+  public function __construct($user, $companyName, $jobTitle, $jobLink, $appStatus){
+    $this->user = $user;
+    $this->companyName = $companyName;
+    $this->jobTitle = $jobTitle;
+    $this->jobLink = $jobLink
+    $this->appStatus = $appStatus
+  }
+}
+
 ?>

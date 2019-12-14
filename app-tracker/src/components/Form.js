f01c047f7faa15fb1ff8d36361e41cd0775e66a8
ashbrick <ashleycbrickhouse@gmail.com>
2019-12-13 16:29:20 -0800

    created constructor/state for form component

---
+++ app-tracker/src/components/Form.js
@@ -0,0 +1,27 @@
+// =============================
+// DEPENDENCIES
+// =============================
+// packages
+import React from 'react'
+
+// =============================
+// COMPONENT CLASS
+// =============================
+class Form extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      id: null, //need this for update
+      user: '',
+      companyName: '',
+      jobTitle: '',
+      jobLink: '',
+      appStatus: ''
+    } //closing for state
+  } //closing for constructor
+
+// ==============
+// HANDLERS
+// ==============
+
+} //closing for Form component

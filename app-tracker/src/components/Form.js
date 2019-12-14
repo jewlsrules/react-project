c7d8e3c77f450b066834da421ceab1381f963506
ashbrick <ashleycbrickhouse@gmail.com>
2019-12-13 16:54:10 -0800

    created form page with input fiels, labels, submit button, and methods we'll need including handleChange, handleSubmit, componentDidMount

--- app-tracker/src/components/Form.js
+++ app-tracker/src/components/Form.js
@@ -17,11 +17,67 @@ class Form extends React.Component {
       jobTitle: '',
       jobLink: '',
       appStatus: ''
-    } //closing for state
-  } //closing for constructor
+    } //  closing for state
+  } //  closing for constructor
 
 // ==============
-// HANDLERS
+// HANDLERS/FUNCTIONS
 // ==============
 
-} //closing for Form component
+//  this handles when the form changes
+handleChange = (event) => {
+  this.setState({[event.target.id]:event.target.value})
+}// closing for handleChange
+
+//  this will handle creating and updating new job app.
+handleSubmit = (event) => {
+  event.preventDefault()
+  console.log('submitted')
+} //  closing for handleSubmit
+
+//  this will set state after form is loaded to populate pre-values/data in fields for editing/updating
+componentDidMount() {
+  this.setState({
+    // id: this.props.formInput.id,
+    // user: this.props.formInput.user,
+    // companyName: this.props.formInput.companyName,
+    // jobTitle: this.props.formInput.jobTitle,
+    // jobLink: this.props.formInput.jobLink,
+    // appStatus: this.props.formInput.appStatus
+  })
+}  // closing for componentDidMount
+
+// ==============
+// RENDER
+// ==============
+  render () {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label htmlFor='user'>user</label>
+        <input type='text' value={this.state.user} id='user' onChange={this.handleChange}/>
+
+        <label htmlFor='companyName'>company name</label>
+        <input type='text' value={this.state.companyName} id='cmpanyName' onChange={this.handleChange}/>
+
+        <label htmlFor='jobTitle'>job title</label>
+        <input type='text' value={this.state.jobTitle} id='jobTitle' onChange={this.handleChange}/>
+
+        <label htmlFor='jobLink'>job link</label>
+        <input type='text' value={this.state.jobLink} id='jobLink' onChange={this.handleChange}/>
+
+        <label htmlFor='appStatus'>app status</label>
+        <input type='text' value={this.state.appStatus} id='appStatus' onChange={this.handleChange}/>
+
+        <input type="submit" value="Submit"/>
+      </form>
+
+    ) //closing for return
+  }
+
+
+} //  closing for Form component
+
+// =============================
+// EXPORT
+// =============================
+export default Form

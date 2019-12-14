53c1bff639ce75bf20210f66b8838f0b4c8adbcc
ashbrick <ashleycbrickhouse@gmail.com>
2019-12-13 17:25:35 -0800

    created basic Applications.js framework

---
+++ app-tracker/src/components/Applications.js
@@ -0,0 +1,50 @@
+// =============================
+// DEPENDENCIES
+// =============================
+// packages
+import React from 'react'
+
+// =============================
+// COMPONENT CLASS
+// =============================
+class Applications extends React.Component {
+
+// =============================
+// HANDLERS/FUNCTIONS
+// =============================
+
+//do we need a handleView and handleDelete? I think we're supposed to pull those in
+
+  // ==============
+  // RENDER
+  // ==============
+  render () {
+    return (
+      <div className="Applications">
+
+        <div className="app-info">
+          {this.props.data.user}
+          {this.props.data.companyName}
+          {this.props.data.jobTitle}
+          {this.props.data.jobLink}
+          {this.props.data.appStatus}
+        </div>
+
+        <div className="app-buttons">
+          <ul>
+            <li onClick={()=>
+            {this.props.handleView('edit', this.props.data)}}>edit application</li>
+            <li onClick={()=>
+            {this.props.handleDelete(this.props.data.id)}}>delete</li>
+          </ul>
+        </div>
+        
+      </div>
+    )
+  }
+}
+
+// =============================
+// EXPORT
+// =============================
+export default Applications

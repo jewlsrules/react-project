229abd2386c9c96b756444934f420d015962d3bc
ashbrick <ashleycbrickhouse@gmail.com>
2019-12-14 00:08:05 -0800

    added list of nav links with onClicks for menu

--- app-tracker/src/components/Menu.js
+++ app-tracker/src/components/Menu.js
@@ -8,8 +8,21 @@ import React from 'react'
 // COMPONENT CLASS
 // =============================
 class Menu extends React.Component {
+
+
+// ==============
+// RENDER
+// ==============
   render () {
     return (
+      <div className="menu">
+        <ul>
+          <li onClick={()=>
+          {this.props.handleView('home')}}>home</li>
+          <li onClick={(=>
+          {this.props.handleView('addApp')})}>add application</li>
+        </ul>
+      </div>
 
     )
   }

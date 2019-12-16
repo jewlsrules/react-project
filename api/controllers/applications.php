ff10a34ec37a22caebb48f921ac9a774ca2fc8d5
Jewls Krueger <Juliarkrueger@gmail.com>
2019-12-16 12:27:21 -0800

    changed to action == from action ===

--- api/controllers/applications.php
+++ api/controllers/applications.php
@@ -2,21 +2,21 @@
 include_once __DIR__ . '/../models/application.php';
 header('Content-Type: application/json');
 
-if ($_REQUEST['action'] === 'index') {
+if ($_REQUEST['action'] == 'index') {
   echo json_encode(Applications::all());
-} elseif ($_REQUEST['action'] === 'post') {
+} elseif ($_REQUEST['action'] == 'post') {
   $request_body = file_get_contents('php://input');
   $body_object = json_decode($request_body);
   $new_application = new Application(null, $body_object->user_id, $body_object->company_name, $body_object->job_title, $body_object->job_link, $body_object->app_status);
   $all_applications = Applications::create($new_application);
   echo json_encode($all_applications);
-} elseif ($_REQUEST['action'] === 'update'){
+} elseif ($_REQUEST['action'] == 'update'){
   $request_body = file_get_contents('php://input');
   $body_object = json_decode($request_body);
   $updated_app = new Application($_REQUEST['id'], $body_object->user_id, $body_object->company_name, $body_object->job_title, $body_object->job_link, $body_object->app_status);
   $all_applications Applications::update($updated_post);
   echo json_encode($all_applications);
-} elseif ($_REQUEST['action'] === 'delete') {
+} elseif ($_REQUEST['action'] == 'delete') {
   $all_applications = Applications::delete($_REQUEST['id']);
   echo json_encode($all_applications);
 }

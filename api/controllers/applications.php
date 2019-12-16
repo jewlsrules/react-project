d3b5e8e1d2c418d0a61dc7e336120e9f00e66385
Jewls Krueger <Juliarkrueger@gmail.com>
2019-12-16 12:06:23 -0800

    changed case of the applications.php file routes to troubleshoot

--- api/controllers/applications.php
+++ api/controllers/applications.php
@@ -4,19 +4,19 @@ header('Content-Type: application/json');
 
 if ($_REQUEST['action'] === 'index') {
   echo json_encode(Applications::all());
-} elseif ($_REQUEST['action'] === 'post') {
+} elseif ($_REQUEST['action'] === 'POST') {
   $request_body = file_get_contents('php://input');
   $body_object = json_decode($request_body);
   $new_application = new Application(null, $body_object->user_id, $body_object->company_name, $body_object->job_title, $body_object->job_link, $body_object->app_status);
   $all_applications = Applications::create($new_application);
   echo json_encode($all_applications);
-} elseif ($_REQUEST['action'] === 'update'){
+} elseif ($_REQUEST['action'] === 'UPDATE'){
   $request_body = file_get_contents('php://input');
   $body_object = json_decode($request_body);
   $updated_app = new Application($_REQUEST['id'], $body_object->user_id, $body_object->company_name, $body_object->job_title, $body_object->job_link, $body_object->app_status);
   $all_applications Applications::update($updated_post);
   echo json_encode($all_applications);
-} elseif ($_REQUEST['action'] === 'delete') {
+} elseif ($_REQUEST['action'] === 'DELETE') {
   $all_applications = Applications::delete($_REQUEST['id']);
   echo json_encode($all_applications);
 }
